b3a0421ede3311d38f4d254985c2020c041fb9e2
gejimayu <gianffh@gmail.com>
2017-11-01 00:54:43 +0700

    Connect to database

--- IdentityService/src/logout.java
+++ IdentityService/src/logout.java
@@ -28,6 +28,7 @@ public class logout extends HttpServlet {
 	static final String PASS = "";
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//Read body
 		StringBuffer jb = new StringBuffer();
 		String line = null;
 		try {
@@ -36,6 +37,7 @@ public class logout extends HttpServlet {
 		    jb.append(line);
 		} catch (Exception e) { /*report an error*/ }
 		
+		//Parse string JSON
 		String jsonData = jb.toString();
 		PrintWriter out = response.getWriter();
 		try {
@@ -46,6 +48,56 @@ public class logout extends HttpServlet {
 		catch (Exception e) {
 			e.printStackTrace();
 		}
+		
+	    Statement stmt = null;
+	    Connection conn = null;
+	      
+	    try {
+	    	// Register JDBC driver
+	        Class.forName("com.mysql.jdbc.Driver");
+	          
+	      	// Open a connection
+	        out.println("try to connect");
+	        conn = DriverManager.getConnection(DB_URL, USER, PASS);
+	        out.println("success");
+	          
+	        // Execute SQL query
+	        stmt = (Statement) conn.createStatement();
+	        String sql;
+	        sql = "SELECT * from accesstoken";
+	        ResultSet rs = stmt.executeQuery(sql);
+	          
+	        while (rs.next()) {
+	        	String name = rs.getString("token");
+	        	out.println("Name : " + name);
+	        }
+	          
+	        rs.close();
+	        stmt.close();
+	        conn.close();
+	    } 
+	    catch(SQLException se) {
+	    	//Handle errors for JDBC
+	    	se.printStackTrace();
+	    } 
+	    catch(Exception e) {
+	      	//Handle errors for Class.forName
+	      	e.printStackTrace();
+	    } 
+	    finally {
+	    	//finally block used to close resources
+	        try {
+	           if(stmt!=null)
+	              stmt.close();
+	        } catch(SQLException se2) {
+	        } // nothing we can do
+	        try {
+	           if(conn!=null)
+	           conn.close();
+	        } catch(SQLException se) {
+	             se.printStackTrace();
+	        } //end finally try
+	    }
 	}
 
 }
