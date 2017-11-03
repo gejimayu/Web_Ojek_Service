7a0973f9a8e7d1fd682bb9ad6be830d33ab5f62d
Erfandi Suryo Putra <13515145@std.stei.itb.ac.id>
2017-11-03 19:17:06 +0700

    jsp dan servlet register

---
+++ IdentityService/src/register.java
@@ -0,0 +1,136 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONObject;
+
+import java.sql.*;
+import com.mysql.jdbc.Statement;
+
+/**
+ * Servlet implementation class logout
+ */
+@WebServlet("/register")
+public class register extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	// JDBC driver name and database URL
+	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+	static final String DB_URL = "jdbc:mysql://localhost:3306/ojekaccount";
+
+	//  Database credentials
+	static final String USER = "root";
+	static final String PASS = "";
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		StringBuffer jb = new StringBuffer();
+		String line = null;
+		String fullname = null;
+		String username = null;
+		String email = null;
+		String password = null;
+		String confirmpassword = null;
+		String phonenumber = null;
+		String driverstatus = null;
+		
+		//Read body
+		try {
+		  BufferedReader reader = request.getReader();
+		  while ((line = reader.readLine()) != null)
+		    jb.append(line);
+		} catch (Exception e) { /*report an error*/ }
+		
+		//Parse string JSON
+		String jsonData = jb.toString();
+		PrintWriter out = response.getWriter();
+		try {
+			JSONObject jsonObject = new JSONObject(jsonData); // put "String"
+			fullname = jsonObject.getString("fullname");
+			username = jsonObject.getString("username");
+			email = jsonObject.getString("email");
+			password = jsonObject.getString("password");
+			confirmpassword = jsonObject.getString("confirmpassword");
+			phonenumber = jsonObject.getString("phonenumber");
+			driverstatus = jsonObject.getString("driverstatus");			
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		//Connect to database
+	    Statement stmt = null;
+	    Connection conn = null;
+	    
+	    try {
+	    	// Register JDBC driver
+	        Class.forName("com.mysql.jdbc.Driver");
+	          
+	      	// Open a connection
+	        out.println("try to connect");
+	        conn = DriverManager.getConnection(DB_URL, USER, PASS);
+	        out.println("success");
+	          
+	        stmt = (Statement) conn.createStatement();
+	        //String sql;
+	        /*
+	        //Validate token
+	        sql = "SELECT * FROM accesstoken WHERE token = '" + usertoken + "'";
+	        ResultSet rs =stmt.executeQuery(sql);
+	        if (!rs.next()) { //token exists
+	        	response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+	        	return;
+	        }
+	        
+	        // Execute Delete Query
+	        sql = "DELETE FROM accesstoken WHERE token = '" + usertoken + "'";
+	        stmt.executeUpdate(sql);
+	        response.setStatus(HttpServletResponse.SC_OK );
+	        */
+			ResultSet rs = stmt.executeQuery("select * from ojekaccount where username='"+username+"' or email='"+email+"'");
+	        int count=0;
+	        while(rs.next()){
+	        	count++;
+	        }
+		    if((count == 0) && (fullname != "") && (username != "") && (email != "") && (password != "") && (password == confirmpassword) && (phonenumber != "")) {
+		    	out.println("welcome "+fullname);
+		    	stmt.executeQuery("INSERT INTO user (username, password, name, phonenumber, email, driverstatus) VALUES ('"+username+"', '"+password+"', '"+fullname+"', '"+phonenumber+"', '"+email+"', '"+driverstatus+"');");
+		    }
+		    else {
+		        response.sendRedirect("http://localhost:8080/WebApp/register");
+		        out.println("Input tidak valid");
+		    }
+		    
+	        stmt.close();
+	        conn.close();
+	    } 
+	    catch(SQLException se) {
+	    	//Handle errors for JDBC
+	    	response.setStatus(HttpServletResponse.SC_BAD_REQUEST );
+	    	se.printStackTrace();
+	    } 
+	    catch(Exception e) {
+	      	//Handle errors for Class.forName
+	      	e.printStackTrace();
+	    } 
+	    finally {
+	    	//finally block used to close resources
+	        try {
+	           if(stmt!=null)
+	              stmt.close();
+	        } catch(SQLException se2) {
+	        } // nothing we can do
+	        try {
+	           if(conn!=null)
+	        	   conn.close();
+	        } catch(SQLException se) {
+	             se.printStackTrace();
+	        } //end finally try
+	    }
+	}
+
+}
---
+++ WebApp/WebContent/register.jsp
@@ -0,0 +1,66 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Register</title>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+<script src="assets/javascript/login.js"></script>
+<script>
+	function submitForm(thisObj, thisEvent) {
+		var fullname = $('#fullname').val();
+		var username = $('#username').val();
+		var email = $('#email').val();
+		var password = $('#password').val();
+		var confirmpassword = $('#confirmpassword').val();
+		var phonenumber = $('#phonenumber').val();
+		var driverstatus = $('#driverstatus').val();
+			console.log(fullname, username, email, password, confirmpassword, phonenumber, driverstatus);
+		var myData = {
+			"ojekaccount": {
+		    "fullname": fullname,
+		    "username": username,
+		    "email": email,
+		    "password": password,
+		    "confirmpassword": confirmpassword,
+		    "phonenumber": phonenumber,
+		    "driverstatus": driverstatus,
+		}
+		};
+		$.ajax({
+			type: "POST",
+			url: "http://localhost:8081/IdentityService/register",
+		    data: {
+		      jsonData: JSON.stringify(myData)
+		    },
+		    dataType: "json",
+		    contentType: 'application/json'
+		  });
+		  console.log(JSON.stringify(myData));
+		  return false;
+	}
+</script>
+
+</head>
+<body>
+	<center>
+		<form method="post">
+			<center style="font-size: 30px"><b>SIGNUP</b></center><br>
+			<table>
+				<tr><td>Your Name</td><td colspan="2"><input id="fullname" name="fullname" type="text" maxlength="20"/></td></tr>
+				<tr><td>Username</td><td><input id="username" name="username" type="text" type="text" maxlength="20"/></td></tr>
+				<tr><td>Email</td><td><input id="email" name="email" type="text" maxlength="20"/></td></tr>
+				<tr><td>Password</td><td colspan="2"><input id="password" name="password" type="password" maxlength="20"/></td></tr>
+				<tr><td>Confirm Password</td><td colspan="2"><input id="confirmpassword" name="confirmpassword" type="password" maxlength="20"/></td></tr>
+				<tr><td>Phone Number</td><td colspan="2"><input id="phonenumber" name="phonenumber" type="text" maxlength="12"/></td></tr>
+				<input type="hidden" name="driverstatus" value="0" />
+				<tr><td colspan="3"><input id="driverstatus" type="checkbox" name="driverstatus" value="1" />Also sign me up as a driver!</td></tr>
+				<tr><td colspan="3" height="10"></td></tr>			
+				<tr><td><a href=http://localhost:8080/WebApp/login>Already have an account?</a></td>
+				<td align="right" colspan="2"><input type='submit' value="REGISTER" onclick="return submitForm(this, event)"></td></tr>
+			</table>
+		</form>
+	</center>
+</body>
+</html>
