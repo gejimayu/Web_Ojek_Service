cb669bdae167c3a77a0bdf6c1637196fb9d18b22
gejimayu <gianffh@gmail.com>
2017-11-07 20:13:50 +0700

    merge with master(2)

--- OjekService/src/org/java/ojekonline/webservice/DataOjekPublisher.java
+++
@@ -1,8 +0,0 @@
-package org.java.ojekonline.webservice;
-import javax.xml.ws.Endpoint;
-
-public class DataOjekPublisher {
-	public static void main(String[] args) {
-		Endpoint.publish("http://localhost:8089/WS/DataOjek", new DataOjekImpl());
-	}
-}
