251c0bb21b35ff19d14c55b52eceb58828ec9de6
gejimayu <gianffh@gmail.com>
2017-11-07 20:10:39 +0700

    Update database schema

--- IdentityService/account.sql
+++ IdentityService/account.sql
@@ -3,26 +3,22 @@ DROP TABLE IF EXISTS accesstoken;
 
 CREATE TABLE userdata (
 	id_user int NOT NULL AUTO_INCREMENT,
-	name varchar(50),
 	username varchar(20),
-	email varchar(100),
 	password varchar(50),
-	phone_number varchar(20),
-	driver_status varchar(10),
 	PRIMARY KEY (id_user)
 );
 
-INSERT INTO userdata(name, username, email, password, phone_number, driver_status) 
-VALUES 	("Pika1", "pikapika",  "cobacoba@gmail.com", "asdbc", "085723289999", "true"),
-		("Pika2", "pikapika2",  "cobacoba@gmail.com", "asdbc", "085723289999", "true"),
-		("Pika3", "pikapika3", "cobacoba3@gmail.com", "asdbc", "085723289999", "true"),
-		("Pika4", "pikapika4", "cobacoba4@gmail.com", "asdbc", "08239293929", "true"),
-		("Pika5", "pikapika5", "cobacoba5@gmail.com", "asdbc", "08239293929", "true"),
-		("Pika6", "pikapika6",  "cobacoba6@gmail.com", "asdbc", "08239293929", "true"),
-		("Pika7", "pikapika7", "cobacoba@gmail.com", "asdbc", "085723289999", "false"),
-		("Pika8", "pikapika8", "cobacoba3@gmail.com", "asdbc", "085723289999", "false"),
-		("kuningan", "pika", "tes@asd.ib", "asdbc", "123412341234", "true"),
-		("asem", "pikalol", "asd@gmx.s", "asdbc", "12341234", "true");
+INSERT INTO userdata(username, password)
+VALUES 	("pikapika", "asdbc"),
+		("pikapika2", "asdbc"),
+		("pikapika3","asdbc"),
+		("pikapika4","asdbc"),
+		("pikapika5","asdbc"),
+		("pikapika6", "asdbc"),
+		("pikapika7","asdbc"),
+		("pikapika8","asdbc"),
+		("pika", "asdbc"),
+		("pikalol", "asdbc");
 
 CREATE TABLE accesstoken (
 	id_user int NOT NULL,
